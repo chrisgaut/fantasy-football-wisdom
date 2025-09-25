20dca0751f1c4dce1037fd09346fc2114cb5c69d
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2025-09-24 19:37:45 -0700

    Unnest players within dataframe

--- download-sleeper-data.R
+++ download-sleeper-data.R
@@ -3,6 +3,7 @@
 
 # TODO:
 # - Seperate script for functions?
+# - Team metadata function, storing settings and metadata from rosters pull
 ########################################
 
 # Load packages ----------------------------------------------------------------
@@ -27,9 +28,36 @@ download_roster_data <- function(request){
   data <- resp |> resp_body_json()
   df <- do.call(rbind, data) |> as.data.frame()
   
-  # Drop unused data
+  # Drop unused data and reorganize columns
   df <- df %>%
-    select(-co_owners, -keepers, -league_id, -metadata, -player_map, -taxi)
+    select(owner_id, roster_id, players, -co_owners, -keepers, -league_id, -metadata, -player_map, -taxi, -reserve, -settings, -starters)
+  
+  # Expand players
+  df <- df %>%
+    unnest(players)
+  
+  # Return data frame
+  return(df)
+}
+
+download_starters_data <- function(request){
+  # Request
+  req <- request(request)
+  
+  # Perform request
+  resp <- req_perform(req)
+  
+  # Create dataframe
+  data <- resp |> resp_body_json()
+  df <- do.call(rbind, data) |> as.data.frame()
+  
+  # Drop unused data and reorganize columns
+  df <- df %>%
+    select(owner_id, roster_id, starters, -co_owners, -keepers, -league_id, -metadata, -player_map, -taxi, -reserve, -settings, -players)
+  
+  # Expand players
+  df <- df %>%
+    unnest(starters)
   
   # Return data frame
   return(df)
@@ -59,6 +87,9 @@ download_player_data <- function(request){
 # Download data ----------------------------------------------------------------
 rosters_call <- paste0("https://api.sleeper.app/v1/league/", config$sleeper_league_id, "/rosters")
 rosters <- download_roster_data(rosters_call)
+
+starters_call <- paste0("https://api.sleeper.app/v1/league/", config$sleeper_league_id, "/rosters")
+starters <- download_roster_data(starters_call)
 
 player_call <- "https://api.sleeper.app/v1/players/nfl"
 players <- download_player_data(player_call)
